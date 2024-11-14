f733903976fab9f411092ca2c5bc5605a49dda2b
Santeri <santeri.ora@tuni.fi>
2024-11-14 16:59:49 +0200

    Implemented /state endpoint tests

--- tests/api.test.js
+++ tests/api.test.js
@@ -5,17 +5,68 @@ const chai = use(chaiHttp);
 
 const exposedUrl = "http://localhost:8197";
 
-describe("API route test", () => {
-  it("should return the initial state", (done) => {
-    chai.request
-      .execute(exposedUrl)
-      .get("/state")
-      .auth("testuser", "testpassword")
-      .end((err, res) => {
-        expect(res).to.have.status(200);
-        expect(res.text).to.be.a("string");
-        expect(res.text).to.equal("INIT");
-        done();
-      });
+describe("API route tests", () => {
+  describe("/state endpoint tests", () => {
+    it("Should return INIT before login", (done) => {
+      chai.request
+        .execute(exposedUrl)
+        .get("/state")
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.text).to.be.a("string");
+          expect(res.text).to.equal("INIT");
+          done();
+        });
+    });
+
+    it("Should return RUNNING when logged in", (done) => {
+      chai.request
+        .execute(exposedUrl)
+        .get("/state")
+        .auth("testuser", "testpassword")
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.text).to.be.a("string");
+          expect(res.text).to.equal("RUNNING");
+          done();
+        });
+    });
+
+    it("Should return PAUSED after PUT with PAUSED", (done) => {
+      chai.request
+        .execute(exposedUrl)
+        .put("/state")
+        .send({ state: "PAUSED" })
+        .auth("testuser", "testpassword")
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.text).to.be.a("string");
+          expect(res.text).to.equal("PAUSED");
+          done();
+        });
+    });
+
+    it("Should change state to SHUTDOWN and stop responding", (done) => {
+      chai.request
+        .execute(exposedUrl)
+        .put("/state")
+        .send({ state: "SHUTDOWN" })
+        .auth("testuser", "testpassword")
+        .end((err, res) => {
+          expect(res).to.have.status(200);
+          expect(res.text).to.equal("SHUTDOWN");
+
+          setTimeout(() => {
+            chai.request
+              .execute(exposedUrl)
+              .get("/state")
+              .end((err, res) => {
+                expect(err).to.not.be.null;
+                expect(res).to.be.undefined;
+                done();
+              });
+          }, 10000);
+        });
+    });
   });
 });
